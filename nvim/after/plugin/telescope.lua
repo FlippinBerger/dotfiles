d83c9e628d0477353d927d1a89f0b321425cc6ea
Chris Berger <flippinberger@gmail.com>
2025-01-21 22:07:10 -0700

    Take off the git file search kind for telescope

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -11,6 +11,6 @@ require("telescope").setup({
 })
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
+-- vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 vim.keymap.set("n", "<leader>st", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>b", builtin.buffers, {})
