ea88dcb798e14f6dc8dd9e59713109e1734d08f4
Chris Berger <flippinberger@gmail.com>
2025-08-09 12:02:57 -0600

    Add back in autocomplete :facepalm:

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -26,6 +26,27 @@ vim.lsp.enable("html")
 nvimlsp.tailwindcss.setup({})
 vim.lsp.enable("tailwindcss")
 
+local cmp = require("cmp")
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+	sources = {
+		{ name = "nvim_lsp" },
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Enter key confirms completion item
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		-- Ctrl + space triggers completion menu
+		["<C-Space>"] = cmp.mapping.complete(),
+	}),
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+})
+
 -- lspconfig.gdscript.setup({
 -- 	capabilities = lsp_capabilities,
 -- })
--- nvim/lua/flippin/plugins.lua
+++ nvim/lua/flippin/plugins.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
 	{ "williamboman/mason.nvim" },
 	{ "williamboman/mason-lspconfig.nvim" },
 	{ "hrsh7th/nvim-cmp" },
+	{ "hrsh7th/cmp-nvim-lsp" },
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
