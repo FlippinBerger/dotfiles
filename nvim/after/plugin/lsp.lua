b9e35a6830ab05792b478dc3666b4ca6bd2a590a
Chris Berger <flippinberger@gmail.com>
2025-08-08 18:00:18 -0600

    Clean up lsp config to utilize builtin functionality

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,116 +4,35 @@ vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>")
 vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
 vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
 
-vim.api.nvim_create_autocmd("LspAttach", {
-	desc = "LSP actions",
-	callback = function(event)
-		local opts = { buffer = event.buf }
-
-		-- these will be buffer-local keybindings
-		-- because they only work if you have an active language server
-
-		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-		vim.keymap.set("n", "<leader>vca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	end,
-})
-
-local default_setup = function(server)
-	require("lspconfig")[server].setup({
-		capabilities = lsp_capabilities,
-	})
-end
-
 require("mason").setup({})
 require("mason-lspconfig").setup({
 	ensure_installed = { "lua_ls", "rust_analyzer", "eslint", "jsonls", "ts_ls", "volar" },
-	handlers = {
-		default_setup,
-	},
 })
 
-local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local nvimlsp = require("lspconfig")
 
-local lspconfig = require("lspconfig")
+nvimlsp.lua_ls.setup({})
+vim.lsp.enable("lua_ls")
 
--- enable tailwind css lsp
-lspconfig.tailwindcss.setup({
-	-- on_attach = on_attach,
-	capabilities = lsp_capabilities,
-})
+nvimlsp.gopls.setup({})
+vim.lsp.enable("gopls")
 
--- lspconfig.ts_ls.setup({
---     capabilities = lsp_capabilities
+nvimlsp.ts_ls.setup({})
+vim.lsp.enable("ts_ls")
+
+nvimlsp.html.setup({})
+vim.lsp.enable("html")
+
+nvimlsp.tailwindcss.setup({})
+vim.lsp.enable("tailwindcss")
+
+-- lspconfig.gdscript.setup({
+-- 	capabilities = lsp_capabilities,
 -- })
---
--- If you are using mason.nvim, you can get the ts_plugin_path like this
-local mason_registry = require("mason-registry")
--- print(mason_registry.get_package("vue-language-server"):get_install_path())
-local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-	.. "/node_modules/@vue/language-server"
-
-lspconfig.ts_ls.setup({
-	init_options = {
-		plugins = {
-			{
-				name = "@vue/typescript-plugin",
-				location = vue_language_server_path,
-				-- location = '/Users/chris/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server/',
-				languages = { "vue" },
-			},
-		},
-	},
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-})
-
--- No need to set `hybridMode` to `true` as it's the default value
-lspconfig.volar.setup({})
-
-lspconfig.dartls.setup({})
-
-lspconfig.html.setup({
-	capabilities = lsp_capabilities,
-})
-
-lspconfig.lua_ls.setup({
-	capabilities = lsp_capabilities,
-})
-
-lspconfig.gdscript.setup({
-	capabilities = lsp_capabilities,
-})
 
 -- alllow godot to send nvim client-server commands
 -- this starts a listening server at ~/.cache/nvim/server.pipe
-local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
-if not vim.loop.fs_stat(pipepath) then
-	vim.fn.serverstart(pipepath)
-end
-
-local cmp = require("cmp")
-require("luasnip.loaders.from_vscode").lazy_load()
-
-cmp.setup({
-	sources = {
-		{ name = "nvim_lsp" },
-	},
-	mapping = cmp.mapping.preset.insert({
-		-- Enter key confirms completion item
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
-
-		-- Ctrl + space triggers completion menu
-		["<C-Space>"] = cmp.mapping.complete(),
-	}),
-	snippet = {
-		expand = function(args)
-			require("luasnip").lsp_expand(args.body)
-		end,
-	},
-})
+-- local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+-- if not vim.loop.fs_stat(pipepath) then
+-- 	vim.fn.serverstart(pipepath)
+-- end
--- nvim/lua/flippin/plugins.lua
+++ nvim/lua/flippin/plugins.lua
@@ -41,11 +41,9 @@ require("lazy").setup({
 		"stevearc/conform.nvim",
 	},
 	-- lsp stuff
-	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
 	{ "neovim/nvim-lspconfig" },
 	{ "williamboman/mason.nvim" },
 	{ "williamboman/mason-lspconfig.nvim" },
-	{ "hrsh7th/cmp-nvim-lsp" },
 	{ "hrsh7th/nvim-cmp" },
 	{
 		"L3MON4D3/LuaSnip",
