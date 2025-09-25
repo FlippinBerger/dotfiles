0e13b614f51db7645560dd70692005b2ecd86084
Chris Berger <flippinberger@gmail.com>
2025-09-25 10:41:12 -0600

    Add current line number and update gutter colors

--- nvim/lua/flippin/core/settings.lua
+++ nvim/lua/flippin/core/settings.lua
@@ -3,6 +3,7 @@ vim.o.expandtab = true
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 
+vim.wo.number = true
 vim.wo.relativenumber = true
 
 vim.o.hlsearch = false
--- nvim/lua/flippin/plugins/colorscheme.lua
+++ nvim/lua/flippin/plugins/colorscheme.lua
@@ -20,8 +20,20 @@ return {
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+			sidebars = "normal",
+		},
 		config = function()
+			require("tokyonight").setup({
+				-- ... your other settings
+				on_colors = function(colors)
+					colors.fg_gutter = "#3d3e40"
+				end,
+				on_highlights = function(hl, colors)
+					-- Change the standard line number color to yellow
+					hl.LineNr = { fg = "#20c203" }
+				end,
+			})
 			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
